85c375e9bfdffced31b070d417c8d7624282dd16
Mauricio Cirelli <mauricio.cirelli@quantatec.com.br>
2016-04-07 09:05:07 -0300

    Adding Turing's Logo

--- drivers/video/logo/logo.c
+++ drivers/video/logo/logo.c
@@ -96,6 +96,10 @@ const struct linux_logo * __init_refok fb_find_logo(int depth)
 		/* M32R Linux logo */
 		logo = &logo_m32r_clut224;
 #endif
+#ifdef CONFIG_LOGO_TURING_CLUT224
+		/* Turing Logo */
+		logo = &logo_turing_clut224;
+#endif
 	}
 	return logo;
 }
